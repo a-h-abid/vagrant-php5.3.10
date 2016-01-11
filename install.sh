80d30a2719deeb9ab8863f1178f428479a5162ae
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2016-01-11 18:17:08 +0600

    install.sh update

--- install.sh
+++ install.sh
@@ -30,10 +30,6 @@ EOF
 echo "--- Enabling mod-rewrite ---"
 sudo a2enmod rewrite
 
-echo "--- Setting document root ---"
-sudo rm -rf /var/www
-sudo ln -fs /vagrant/public /var/www
-
 
 echo "--- What developer codes without errors turned on? Not you, master. ---"
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
